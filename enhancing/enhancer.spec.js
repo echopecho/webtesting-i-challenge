a1c566d29c82d12cf90968bae1e287af8315322e
echopecho <pbsmithtbn@gmail.com>
2019-04-10 20:53:20 -0500

    Add basic test for repair function
    To see if things are working, I added a simple test to make sure the object return has 100 mapped to the durability key.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('the enhancer module', () => {
+
+  describe('the repair function', () => {
+    const expected = {
+      name: expect.anything(),
+      durability: 100,
+      enhancement: expect.anything()
+    };
+    const item = {
+      name: 'Steve',
+      durability: 50,
+      enhancement: 10
+    }
+
+    it('should return an item with 100 durability', () => {
+      expect(enhancer.repair(item)).toEqual(expect.objectContaining({...item, durability: 100}));
+    });
+  });
+});
